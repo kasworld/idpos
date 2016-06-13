2de38bf31b0507030e65f2dd7868f09d21db2d08
Feram <feram@feram.co>
2016-06-13 00:36:19 +0000

    Fix typos

--- idpos2m/idpos.go
+++ idpos2m/idpos.go
@@ -9,7 +9,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// postioned object managment in 2d space
+// postioned object management in 2d space
 package idpos2m
 
 import (
